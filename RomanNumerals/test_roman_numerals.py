2a1bf9a34c68a279de8a8bb70e581deba6247032
Richárd Vaszi <64356862+CarRycharD@users.noreply.github.com>
2023-03-20 15:00:48 +0100

    Increased the coverage of the tests

--- RomanNumerals/test_roman_numerals.py
+++ RomanNumerals/test_roman_numerals.py
@@ -1,26 +1,20 @@
+import random
 import unittest
 
-from roman_numerals import RomanNumeral
+from roman_numerals import RomanNumeralConverter
 
 class TestRomanNumeral(unittest.TestCase):
     def setUp(self):
-        self.rn = RomanNumeral()
+        # Create an instance of the RomanNumeral class to use in the tests
+        self.roman_numeral_converter = RomanNumeralConverter()
 
-    def test_integer_to_roman(self):
-        self.assertEqual(self.rn.integer_to_roman(2), 'II')
-        self.assertEqual(self.rn.integer_to_roman(13), 'XIII')
-        self.assertEqual(self.rn.integer_to_roman(78), 'LXXVIII')
-        self.assertEqual(self.rn.integer_to_roman(789), 'DCCLXXXIX')
-        self.assertEqual(self.rn.integer_to_roman(1999), 'MCMXCIX')
-        self.assertEqual(self.rn.integer_to_roman(2022), 'MMXXII')
+    def test_roman_conversion(self):
+        for i in range(100000):
+            number = random.randint(1, 3999)
+            roman = self.roman_numeral_converter.integer_to_roman(number)
+            integer = self.roman_numeral_converter.roman_to_integer(roman)
+            self.assertEqual(integer, number)
 
-    def test_roman_to_integer(self):
-        self.assertEqual(self.rn.roman_to_integer('XXI'), 21)
-        self.assertEqual(self.rn.roman_to_integer('CXXXV'), 135)
-        self.assertEqual(self.rn.roman_to_integer('DXCII'), 592)
-        self.assertEqual(self.rn.roman_to_integer('MCMXCVI'), 1996)
-        self.assertEqual(self.rn.roman_to_integer('MMXXII'), 2022)
-        self.assertEqual(self.rn.roman_to_integer('MMMCMXCIX'), 3999)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
