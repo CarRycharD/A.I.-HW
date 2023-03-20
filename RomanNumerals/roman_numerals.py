a3ef31f52708d0cc77bf6ab7f99d2cee16ce0d34
Richárd Vaszi <64356862+CarRycharD@users.noreply.github.com>
2023-03-20 14:59:00 +0100

    refactored for better edgecase handling

--- RomanNumerals/roman_numerals.py
+++ RomanNumerals/roman_numerals.py
@@ -1,4 +1,4 @@
-class RomanNumeral:
+class RomanNumeralConverter:
     def __init__(self):
         # Create a dictionary to map Roman numerals to integers
         self.roman_to_int = {
@@ -16,9 +16,15 @@ class RomanNumeral:
             'IV': 4,
             'I': 1
         }
-
+ 
     def integer_to_roman(self, number: int) -> str:
         # Convert an integer to a Roman numeral
+
+        if number < 1 or number > 3999:
+            raise ValueError('Input must be between 1 and 3999.')
+        if not isinstance(number, int):
+            raise TypeError('Input must be an integer.')
+        
         roman_numeral = ""
         for symbol, value in self.roman_to_int.items():
             count = number // value
@@ -28,13 +34,22 @@ class RomanNumeral:
 
     def roman_to_integer(self, roman: str) -> int:
         # Convert a Roman numeral to an integer
+
+        if not roman:
+            raise ValueError("Input must not be empty")
+        if not all(symbol in self.roman_to_int for symbol in roman):
+            raise ValueError("Invalid Roman numeral")
+        
         total = 0
-        for i, symbol in enumerate(roman):
+        last_value = 0
+        for symbol in roman:
             value = self.roman_to_int[symbol]
-            if i < len(roman) - 1 and value < self.roman_to_int[roman[i + 1]]:
-                # Subtract value if the current symbol is smaller than the next symbol
-                total -= value
+            if value > last_value:
+                total += value - 2 * last_value
             else:
-                # Add value if the current symbol is greater than or equal to the next symbol
                 total += value
+            last_value = value
+        if self.integer_to_roman(total) != roman:
+            raise ValueError("Invalid Roman numeral")
         return total
+
