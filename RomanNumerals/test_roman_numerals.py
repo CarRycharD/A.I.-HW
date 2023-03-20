9ba4249c127f72a4527439c9e58c494edd060480
Richárd Vaszi <64356862+CarRycharD@users.noreply.github.com>
2023-03-20 15:04:46 +0100

    added tests for edge cases

--- RomanNumerals/test_roman_numerals.py
+++ RomanNumerals/test_roman_numerals.py
@@ -3,6 +3,7 @@ import unittest
 
 from roman_numerals import RomanNumeralConverter
 
+
 class TestRomanNumeral(unittest.TestCase):
     def setUp(self):
         # Create an instance of the RomanNumeral class to use in the tests
@@ -14,6 +15,37 @@ class TestRomanNumeral(unittest.TestCase):
             roman = self.roman_numeral_converter.integer_to_roman(number)
             integer = self.roman_numeral_converter.roman_to_integer(roman)
             self.assertEqual(integer, number)
+
+        self.assertRaises(ValueError, self.roman_numeral_converter.roman_to_integer, 'MMMM')
+        self.assertRaises(ValueError, self.roman_numeral_converter.roman_to_integer, '')
+        self.assertRaises(ValueError, self.roman_numeral_converter.roman_to_integer, 'CD1X')
+        self.assertRaises(ValueError, self.roman_numeral_converter.roman_to_integer, 'XVX')
+
+
+if __name__ == "__main__":
+    unittest.main()
+import random
+import unittest
+
+from roman_numerals import RomanNumeralConverter
+
+
+class TestRomanNumeral(unittest.TestCase):
+    def setUp(self):
+        # Create an instance of the RomanNumeral class to use in the tests
+        self.roman_numeral_converter = RomanNumeralConverter()
+
+    def test_roman_conversion(self):
+        for i in range(100000):
+            number = random.randint(1, 3999)
+            roman = self.roman_numeral_converter.integer_to_roman(number)
+            integer = self.roman_numeral_converter.roman_to_integer(roman)
+            self.assertEqual(integer, number)
+
+        self.assertRaises(ValueError, self.roman_numeral_converter.roman_to_integer, 'MMMM')
+        self.assertRaises(ValueError, self.roman_numeral_converter.roman_to_integer, '')
+        self.assertRaises(ValueError, self.roman_numeral_converter.roman_to_integer, 'CD1X')
+        self.assertRaises(ValueError, self.roman_numeral_converter.roman_to_integer, 'XVX')
 
 
 if __name__ == "__main__":
